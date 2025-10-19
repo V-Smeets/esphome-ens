34bb9829bb43dcbf4706ba398a7f89796c319414
Vincent Smeets <Vincent.VSmeets@GMail.com>
2025-10-19 21:50:29 +0200

    Renamed unique_id() to get_object_id_hash()

--- components/omnik_base/omnik_base.cpp
+++ components/omnik_base/omnik_base.cpp
@@ -236,7 +236,7 @@ void dump_config(const char *const tag, std::string prefix,
               state_class_to_string(sensor->get_state_class()));
   dump_config(tag, prefix, "Accuracy Decimals",
               sensor->get_accuracy_decimals());
-  dump_config(tag, prefix, "Unique ID", sensor->unique_id());
+  dump_config(tag, prefix, "Unique ID", sensor->get_object_id_hash());
   dump_config(tag, prefix, "Force Update",
               sensor->get_force_update() ? "true" : "false");
 }
@@ -252,7 +252,7 @@ void dump_config(const char *const tag, std::string prefix,
   }
   dump_config(tag, prefix, (EntityBase *)text_sensor);
   dump_config(tag, prefix, (EntityBase_DeviceClass *)text_sensor);
-  dump_config(tag, prefix, "Unique ID", text_sensor->unique_id());
+  dump_config(tag, prefix, "Unique ID", text_sensor->get_object_id_hash());
 }
 
 /**
